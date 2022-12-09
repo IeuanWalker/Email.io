aa77ee0414bdb29fabbf477c52024206467aad21
Ieuan Walker <ieuan.walker007@gmail.com>
2022-12-09 15:49:27 +0000

    FileUtil tests

---
+++ Tests/TestProject1/Domain/Utilities/FileUitl_Test.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Utilities;
+
+namespace UnitTests.Domain.Utilities;
+public class FileUitl_Test
+{
+	[Fact]
+	public void IsBase64String_ShouldReturnTrueForValidBase64String()
+	{
+		// Arrange
+		var base64 = "Zm9vYmFy";
+
+		// Act
+		var result = FileUtil.IsBase64String(base64);
+
+		// Assert
+		Assert.True(result);
+	}
+
+	[Fact]
+	public void IsBase64String_ShouldReturnFalseForInvalidBase64String()
+	{
+		// Arrange
+		var base64 = "Zm9vYmFy!";
+
+		// Act
+		var result = FileUtil.IsBase64String(base64);
+
+		// Assert
+		Assert.False(result);
+	}
+}
